0dc550797ad4b77c10c0adcdd6cec2fddb09fc38
Emily Strong <erstrong.06@gmail.com>
2019-02-14 14:07:24 -0500

    update resize script

--- Assignment2/image_resize.py
+++ Assignment2/image_resize.py
@@ -5,6 +5,10 @@ import numpy as np
 import pickle
 import pandas as pd
 
+###################################
+# Resize train images
+###################################
+
 # Resize images to 32x32
 size = (32, 32)
 
@@ -25,6 +29,10 @@ for d in directories:
             if counter % 100 == 0:
                 print(str(counter) + ' images resized')
 
+###################################
+# Resize val images
+###################################
+
 counter = 0
 
 for filename in os.listdir('tiny-imagenet-200/val/images'):
@@ -39,7 +47,9 @@ for filename in os.listdir('tiny-imagenet-200/val/images'):
         if counter % 100 == 0:
             print(str(counter) + ' validation images resized')
 
-# Save images to pickle files
+###################################
+# Save train images to pickle files
+###################################
 
 train_images = []
 train_labels = []
@@ -60,4 +70,31 @@ for filename in os.listdir('data'):
 
 
 pickle.dump(train_images, open( "~/imagenet-200/train_images.pkl", "wb" ) )
-pickle.dump( Y, open( "~/imagenet-200/train_labels.pkl", "wb" ) )
+pickle.dump( train_labels, open( "~/imagenet-200/train_labels.pkl", "wb" ) )
+
+###################################
+# Save val images to pickle files
+###################################
+
+df = pd.read_csv('tiny-imagenet-200/val/val_annotations.txt',sep='\t', header=None)
+df.columns=['file', 'label', 'b1', 'b2', 'b3', 'b4']
+
+
+val_images = []
+val_labels = []
+
+counter = 0
+for filename in os.listdir('val'):
+    if filename[-4:] == 'JPEG':
+        name = 'val/'+filename
+        label = df.loc[df['file'] == filename, 'label'].iloc[0]
+        img = Image.open( name )
+        img.load()
+        val_images.append(np.asarray(img)) 
+        val_labels.append(label)
+        counter += 1
+        if counter % 1000 == 0:
+            print(str(counter) + ' images converted')
+
+pickle.dump(val_images, open( "~/imagenet-200/val_images.pkl", "wb" ) )
+pickle.dump(val_labels, open( "~/imagenet-200/val_labels.pkl", "wb" ) )
